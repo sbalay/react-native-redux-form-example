428ee051f38bfae9df6371a21202f7e0f73b1b38
Sebastian Balay <snbalay@gmail.com>
2017-01-25 21:08:49 -0300

    Stage-2.1 Form level validations

--- src/components/MyForm.js
+++ src/components/MyForm.js
@@ -26,4 +26,24 @@ function MyForm(props) {
   );
 }
 
-export default reduxForm({ form: 'signIn' })(MyForm);
+const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/; // eslint-disable-line
+
+export default reduxForm({
+  form: 'signIn',
+  validate: (values) => {
+    const errors = {};
+    errors.email = !values.email
+      ? 'Email field is required'
+      : !emailRegex.test(values.email)
+      ? 'Email format is invalid'
+      : undefined;
+
+    errors.password = !values.password
+      ? 'Password field is required'
+      : values.password.length < 8
+      ? 'Password must be at least 8 characters long'
+      : undefined;
+
+    return errors;
+  }
+})(MyForm);
