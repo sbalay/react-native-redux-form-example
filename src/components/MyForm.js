1e95761983aba19810fc32033ecb6c4bb3273441
Sebastian Balay <snbalay@gmail.com>
2017-01-26 12:20:04 -0300

    Stage-4 Form with normalized and formatted values

--- src/components/MyForm.js
+++ src/components/MyForm.js
@@ -1,26 +1,79 @@
 import React from 'react';
 import { reduxForm, Field } from 'redux-form';
 import { ScrollView, Text, TouchableOpacity } from 'react-native';
+import moment from 'moment';
 
 import MyTextInput from './MyTextInput';
 import styles from './MyForm.styles';
 
+/**
+ * Automatically adds the dashes required by the specified phone format and limits the input to ten characters
+ */
 const phoneFormatter = (number) => {
   if (!number) return '';
   // NNN-NNN-NNNN
   const splitter = /.{1,3}/g;
-  number = number.replace(/-/g, '').substring(0, 10);
-  return number.toString().substring(0, 7).match(splitter).join('-') + number.substring(7);
+  number = number.substring(0, 10);
+  return number.substring(0, 7).match(splitter).join('-') + number.substring(7);
+};
+
+// remove dashes added by the formatter
+const phoneParser = (number) => number ? number.replace(/-/g, '') : '';
+
+/**
+ * Force after min date
+ */
+const maxDateNormalize = (value, previousValue, values) => {
+  const momentMinDate = moment(values.minDate, 'MM-DD-YYYY', true);
+  const momentMaxDate = moment(value, 'MM-DD-YYYY', true);
+  if (!momentMinDate.isValid() || !momentMaxDate.isValid()) {
+    return value;
+  }
+  if (!momentMaxDate.isAfter(momentMinDate)) {
+    return momentMinDate.add(1, 'd').format('MM-DD-YYYY');
+  }
+  return value;
+};
+
+/**
+ * Force before max date
+ */
+const minDateNormalize = (value, previousValue, values) => {
+  const momentMaxDate = moment(values.maxDate, 'MM-DD-YYYY', true);
+  const momentMinDate = moment(value, 'MM-DD-YYYY', true);
+  if (!momentMinDate.isValid() || !momentMaxDate.isValid()) {
+    return value;
+  }
+  if (!momentMinDate.isBefore(momentMaxDate)) {
+    return momentMaxDate.subtract(1, 'd').format('MM-DD-YYYY');
+  }
+  return value;
 };
 
 function MyForm(props) {
   return (
     <ScrollView style={styles.container} keyboardShouldPersistTaps={'handled'}>
+      <Text style={{ fontWeight: 'bold', marginTop: 20 }}>Phone number</Text>
       <Field
-        name={'phone_number'}
+        name={'phoneNumber'}
         component={MyTextInput}
-        placeholder={'Phone (NNN-NNN-NNNN)'}
+        placeholder={'NNN-NNN-NNNN'}
         format={phoneFormatter}
+        parse={phoneParser}
+      />
+      <Text style={{ fontWeight: 'bold', marginTop: 20 }}>Min date</Text>
+      <Field
+        name={'minDate'}
+        component={MyTextInput}
+        placeholder={'MM-DD-YYYY'}
+        normalize={minDateNormalize}
+      />
+      <Text style={{ fontWeight: 'bold', marginTop: 20 }}>Max date</Text>
+      <Field
+        name={'maxDate'}
+        component={MyTextInput}
+        placeholder={'MM-DD-YYYY'}
+        normalize={maxDateNormalize}
       />
       <TouchableOpacity onPress={props.handleSubmit}>
         <Text style={styles.formSubmit}>Submit!</Text>
